575e254fa71c236fb8b378aa68733e7895b168d1
Arthur <arthuravilla7@gmail.com>
2017-07-02 03:28:04 -0300

    adicionado cors

--- config/server.js
+++ config/server.js
@@ -3,9 +3,11 @@ const port = 3003;
 const bodyParser = require('body-parser');//faz o parse do body das requisicoes
 const express = require('express');
 const server = express(); //instancia do express
+const allowCors = require('./cors');
 
 server.use(bodyParser.urlencoded({ extended: true}));
 server.use(bodyParser.json()); //parse de requisicoes com json como parametro
+server.use(allowCors)//habilita o cors
 
 server.listen(port, function(){
   console.log('Backend is running on port ' + port + '.');
